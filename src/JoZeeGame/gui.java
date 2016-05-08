941615bd52b3e6ffc0ca2f41a08451106348b722
JoCorp <gius.oricchio@gmail.com>
2016-05-08 02:00:36 +0200

    Cude updated
    fewer lines, more documentation

--- src/JoZeeGame/gui.java
+++ src/JoZeeGame/gui.java
@@ -12,42 +12,30 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class gui extends JPanel implements ActionListener {
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
-	//Welt
-	Image img;
-	//Charakter
-	Image img2;
-	int key;
-	int lauf;
-	int X_Bild;
-	Timer time;
+	//Welt und Charakter
+	Image img, img2;
 	//Zur bewegung des Hintergrundbildes notwendig
-	int nx, nx2;
+	int key, lauf, X_Bild, nx, nx2;
+	Timer time;
 	//Zur Bewegung des Charakters notwendig
 	int figur_y = 500;
-	int left = 0;
-	int anzahl = 0;
-	int anzahl2 = 0;
-
+	int left, anzahl, anzahl2 = 0;
 	public gui() {
+		
 		nx = 0;
 		//Größe des Fensters
 		nx2 = 1000;
-
 		key = 0;
 		lauf = 0;
 
 		setFocusable(true);
 		
 		ImageIcon u = new ImageIcon(getClass().getResource("/res/background.jpg"));
-		
 		img = u.getImage();
 		
 		ImageIcon s = new ImageIcon(getClass().getResource("Bowser_On_Skateboard.png"));
-		
 		img2 = s.getImage();
 		
 		addKeyListener(new AL());
@@ -80,16 +68,18 @@ public class gui extends JPanel implements ActionListener {
 		}
 
 		if (getXBild() == 3000 + (anzahl2 * 4000)) {
+			
 			anzahl2++;
 			nx2 = 0;
 		}
 
 		if (getXBild() >= 1000) {
+			
 			f2.drawImage(img, 1000 - nx, 0, null);
-		}
-
-		f2.drawImage(img, 1000 - nx2, 0, null);
 		
+		}
+		
+		f2.drawImage(img, 1000 - nx2, 0, null);
 		f2.drawImage(img2, left, figur_y,100,100, null);
 
 	}
@@ -97,16 +87,26 @@ public class gui extends JPanel implements ActionListener {
 	public void bewegen() {
 		
 		if(lauf != -2){
+			
 			if(left + lauf <= 300){
+				
 				left += lauf;
-			}else {
+			
+			}else{
+				
 				X_Bild += lauf;
 				nx += lauf;
+				
 				nx2 += lauf;
+			
 			}
+		
 		}else{
+			
 			if(left + lauf > 0){
+				
 				left += lauf;
+			
 			}
 			
 		}
